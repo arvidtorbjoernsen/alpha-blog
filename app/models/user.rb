52c6a1735c633bd6bd37d440eb873f4a7c85b205
arvidtorbjoernsen <atorbj@gmail.com>
2020-05-10 20:42:02 +0200

    ls corected

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  validates :username, presence: true, length: { minimum: 3, maximum: 25 }
+  validates :email, presence: true, length: { maximum: 100 }
 end
